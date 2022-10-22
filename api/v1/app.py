10a778acd87e3c5ae7184132887af6f29d79b40d
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-10-22 04:48:39 +0100

    api status

---
+++ api/v1/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+from flask import Flask
+from models import storage
+from os import getenv
+from api.v1.views import app_views
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=getenv('HBNB_API_PORT', '5000'), threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route("/status")
+def get_status():
+    return jsonify({'status': 'OK'})
