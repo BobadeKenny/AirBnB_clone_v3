5c008ce9ec9f671db6a46b39d011f3c866c647a1
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-10-24 22:31:04 +0100

    cors implementation

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,10 +5,12 @@ starts a Flask web application
 
 from flask import Flask
 from flask import jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from os import getenv
 from api.v1.views import app_views
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -23,7 +23,7 @@ def get_reviews(place_id):
 
 
 @app_views.route("/reviews/<review_id>", methods=['GET'], strict_slashes=False)
-def get_place(review_id):
+def get_review(review_id):
     obj = storage.get(Review, review_id)
     if obj is None:
         abort(404)
